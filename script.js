aff0b58347c99632db6c810652f3307f0ea81dcd
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-09-07 10:06:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ const form = document.querySelector("form")
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     footer.classList.toggle("show-footer")
-    
+})
+
+// Pega o valor do Input enquanto o user digita e não deixar digitar caracteres, somente números
+const input = document.getElementById("amount")
+input.addEventListener("input", () => {
+    const CaractersRegex = /\D+/g
+    input.value = input.value.replace(CaractersRegex, "")
 })
