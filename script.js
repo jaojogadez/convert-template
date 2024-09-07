7b483aafd1ce6f74889d8f080d937c7f7f9102c5
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-09-07 11:15:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const footer = document.querySelector("footer")
-const form = document.querySelector("form")
+// Elements
+const form = document.querySelector("form") // formulário
+const input = document.getElementById("amount") // input
+const currency = document.getElementById("currency") // select
+const footer = document.querySelector("footer") // footer
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    footer.classList.toggle("show-footer")
-})
 
-// Pega o valor do Input enquanto o user digita e não deixar digitar caracteres, somente números
-const input = document.getElementById("amount")
+// Pega o valor do Input enquanto o user digita e não deixa digitar caracteres, somente números
 input.addEventListener("input", () => {
     const CaractersRegex = /\D+/g
     input.value = input.value.replace(CaractersRegex, "")
+    footer.classList.remove("show-footer")
 })
+
+// Evento de envio do form
+form.onsubmit = (e) => {
+    e.preventDefault() // previne o recarregamento da tela
+    console.log(currency.value)
+
+    footer.classList.add("show-footer")
+}
+
+//
