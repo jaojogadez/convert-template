ba7880a114f3343e8824cac6680501076fb70ed7
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-09-06 22:41:31 -0300

    update

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const form = document.querySelector("form")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    footer.classList.add("show-footer")
+    footer.classList.toggle("show-footer")
     
 })
